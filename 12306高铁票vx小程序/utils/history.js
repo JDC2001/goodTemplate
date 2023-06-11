a9c9ef5bbb4a63a30d964dfc1886215a2ca404b0
“JDC2001” <2110925527@qq.com>
2023-06-11 13:18:03 +0800

    main

---
+++ 12306高铁票vx小程序/utils/history.js
@@ -0,0 +1,52 @@
+var train = [
+    {
+        begin:"佛山",
+        end:"茂名"
+    },
+    {
+        begin:"广州",
+        end:"茂名"
+    },
+    {
+        begin:"江门",
+        end:"广州"
+    },
+    {
+        begin:"珠海",
+        end:"小榄"
+    },
+    {
+        begin:"小榄",
+        end:"江门"
+    }];
+
+var coach = [
+    {
+        begin:"江门",
+        end:"茂名"
+    },
+    {
+        begin:"广州",
+        end:"茂名"
+    },
+    {
+        begin:"茂名",
+        end:"广州"
+    },
+    {
+        begin:"珠海",
+        end:"中山"
+    },
+    {
+        begin:"中山",
+        end:"江门"
+    }];
+
+
+
+
+
+module.exports = {
+  train: train,
+  coach:coach
+}
